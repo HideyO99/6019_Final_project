db870b5daaffced5bf87aa9b1a20313ba451ce3c
Siraphong <hideyo99@hotmail.com>
2022-12-16 01:58:35 -0500

    init

---
+++ src/MeshObj/cMeshObj.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <string>
+#include <glm/glm.hpp>
+#include <vector>
+//#include <glm/vec3.hpp>
+
+class cMeshObj
+{
+public:
+	cMeshObj();
+	~cMeshObj();
+
+	std::string meshName;
+	std::string instanceName;
+	glm::vec3 position;
+	glm::vec3 rotation;
+	float scale;
+	bool isWireframe;
+	glm::vec4 color_RGBA;
+	bool bUse_RGBA_colour;
+	glm::vec4 specular_colour_and_power;
+	bool isVisible;
+	bool bDoNotLight;
+	std::string textures[8];
+	float textureRatios[8];
+	bool isSkybox;
+	bool isIslandModel;
+
+	std::vector<cMeshObj*> vecChildMesh;
+	
+	cMeshObj* findMeshObjByName(std::string name, bool searchChild = true);
+
+};
+
